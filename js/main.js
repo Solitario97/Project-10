c0e96c9f38789703c54fcb86f16fc7cc76c87ebc
Solitario97 <a.nurmagambetov97@gmail.com>
2023-01-25 22:15:02 +0600

    commitman

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+
+let swiper1 =	document.querySelector(".slider-container");
+
+
 let swiper = new Swiper(".slider-container", {
 	centeredSlides: true,
 	slidesPerView: "auto",
 	loop: true,
 	spaceBetween: 105,
 });
+
