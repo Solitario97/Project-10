fb1ba8ab0e33e6d83dbfd77a9e5f969566ddeab1
Solitario97 <a.nurmagambetov97@gmail.com>
2023-01-26 22:33:36 +0600

    step by step

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 
 let swiper1 =	document.querySelector(".slider-container"),
-	burger	=	document.querySelector(".burger"),
-	close	=	document.querySelector(".menu__close"),
-	menu	=	document.querySelector(".menu");
+		swiper2	=	document.querySelector(".swiper-container"),
+		burger	=	document.querySelector(".burger"),
+		close		=	document.querySelector(".menu__close"),
+		menu		=	document.querySelector(".menu"),
+		playButtonsFirst	=	document.querySelectorAll(".main-slider__play");
 
-let swiper = new Swiper (swiper1, {
+let swiperSlider2 = new Swiper (swiper2, {
+	centeredSlides: true,
+	slidesPerView: 1,
+	loop: true,
+	spaceBetween: 10,
+	effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+	effect:	"fade",
+	navigation: {
+    nextEl: '.btn-left',
+    prevEl: '.btn-right',
+  },
+});
+
+let swiperSlider1 = new Swiper (swiper1, {
 	centeredSlides: true,
 	slidesPerView: "auto",
 	loop: true,
@@ -19,3 +37,14 @@ close.addEventListener("click", () =>{
 	menu.classList.remove("menu--visible"),
 	burger.classList.remove("burger--hidden");
   });
+
+playButtonsFirst.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+		video.play();
+		e.currentTarget.style.display = 'none';
+		setTimeout(() => {
+			video.volume = 0.5;
+		}, 1000);
+	});
+});
